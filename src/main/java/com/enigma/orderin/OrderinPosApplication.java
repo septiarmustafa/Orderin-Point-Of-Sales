e01c53920bb0d64ff95b57a63776e9cbfe1dc722
septiar <septiarmustafa@gmail.com>
2024-03-14 21:19:23 +0700

    Initial commit

---
+++ src/main/java/com/enigma/orderin/OrderinPosApplication.java
@@ -0,0 +1,13 @@
+package com.enigma.orderin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderinPosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderinPosApplication.class, args);
+	}
+
+}
