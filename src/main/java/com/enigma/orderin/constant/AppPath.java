97bc4ffcb162041a9729801546b858ab22b301db
septiar <septiarmustafa@gmail.com>
2024-03-14 21:22:48 +0700

    Setup Entity and Constant

---
+++ src/main/java/com/enigma/orderin/constant/AppPath.java
@@ -0,0 +1,15 @@
+package com.enigma.orderin.constant;
+
+public class AppPath {
+    public final static String API = "/api";
+    public final static String AUTH = "/auth";
+    public final static String REGISTER = "/register";
+    public final static String LOGIN = "/login";
+    public final static String PAGE = "/page";
+    public final static String ID_PRODUCT = "/{id_product}";
+    public final static String ID_ORDER = "/{id_order}";
+    public final static String ADMIN = "/admin";
+    public final static String CASHIER = "/cashier";
+    public final static String PRODUCT = "/product";
+    public final static String ORDER = "/order";
+}
