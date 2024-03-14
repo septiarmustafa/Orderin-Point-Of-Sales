00bbd6bc384b674a556e2029235949cc5a9c2750
septiar <septiarmustafa@gmail.com>
2024-03-14 21:45:59 +0700

    Add order service

---
+++ src/main/java/com/enigma/orderin/service/OrderService.java
@@ -0,0 +1,18 @@
+package com.enigma.orderin.service;
+
+import com.enigma.orderin.dto.request.OrderRequest;
+import com.enigma.orderin.dto.response.OrderResponse;
+import com.enigma.orderin.entity.Order;
+import org.springframework.data.domain.Page;
+
+import java.util.List;
+
+public interface OrderService {
+    OrderResponse createNewOrder(OrderRequest orderRequest);
+    Order getOrderById (Integer id);
+    OrderResponse getOrderResponseById (Integer id);
+    List<OrderResponse> getAllOrder();
+    Page<OrderResponse> getAllWithPagination (Integer page, Integer size, Integer cashierId);
+
+
+}
---
+++ src/main/java/com/enigma/orderin/service/impl/OrderServiceImpl.java
@@ -0,0 +1,230 @@
+package com.enigma.orderin.service.impl;
+
+import com.enigma.orderin.dto.request.OrderDetailRequest;
+import com.enigma.orderin.dto.request.OrderRequest;
+import com.enigma.orderin.dto.response.CashierResponse;
+import com.enigma.orderin.dto.response.OrderDetailResponse;
+import com.enigma.orderin.dto.response.OrderResponse;
+import com.enigma.orderin.dto.response.ProductResponse;
+import com.enigma.orderin.entity.Cashier;
+import com.enigma.orderin.entity.Order;
+import com.enigma.orderin.entity.OrderDetail;
+import com.enigma.orderin.entity.ProductDetail;
+import com.enigma.orderin.repository.OrderDetailRepository;
+import com.enigma.orderin.repository.OrderRepository;
+import com.enigma.orderin.service.CashierService;
+import com.enigma.orderin.service.OrderService;
+import com.enigma.orderin.service.ProductDetailService;
+import jakarta.persistence.criteria.Join;
+import jakarta.persistence.criteria.JoinType;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    final OrderRepository orderRepository;
+    final CashierService cashierService;
+    final ProductDetailService productDetailService;
+    final OrderDetailRepository orderDetailRepository;
+
+    @Transactional(rollbackOn = Exception.class)
+    @Override
+    public OrderResponse createNewOrder(OrderRequest orderRequest) {
+        CashierResponse cashierResponse = cashierService.getById(orderRequest.getCashierId());
+
+        List<OrderDetail> orderDetails = orderRequest.getOrderDetailList().stream().map(orderDetailRequest -> {
+            ProductDetail productDetail = productDetailService.getById(orderDetailRequest.getProductDetailId());
+            return OrderDetail.builder()
+                    .productDetail(productDetail)
+                    .quantity(orderDetailRequest.getQuantity())
+                    .build();
+        }).toList();
+
+        Order order = Order.builder()
+                .cashier(Cashier.builder()
+                        .id(cashierResponse.getId())
+                        .build())
+                .date(LocalDate.now())
+                .orderDetail(orderDetails)
+                .build();
+
+        orderRepository.createNewOrder(order.getDate(), order.getCashier().getId());
+
+        Integer lastInsertOrderId = orderRepository.getLastInsertedId();
+
+        if (lastInsertOrderId  == 0) {
+            throw new RuntimeException("Failed to create the order");
+        }
+
+       Order savedOrder = getOrderById(lastInsertOrderId);
+
+        for (OrderDetailRequest orderDetailRequest : orderRequest.getOrderDetailList()) {
+            Integer productDetailId = orderDetailRequest.getProductDetailId();
+            Integer quantity = orderDetailRequest.getQuantity();
+           orderDetailRepository.createOrderDetail(lastInsertOrderId, productDetailId, quantity);
+        }
+
+        List<OrderDetailResponse> orderDetailResponses = savedOrder.getOrderDetail()
+                .stream()
+                .map(orderDetail -> {
+                    orderDetail.setOrder(savedOrder);
+                    System.out.println(savedOrder);
+
+            ProductDetail currentProductDetail = orderDetail.getProductDetail();
+            currentProductDetail.setStock(currentProductDetail.getStock() - orderDetail.getQuantity());
+            return OrderDetailResponse.builder()
+                    .orderDetailId(orderDetail.getId())
+                    .quantity(orderDetail.getQuantity())
+                    .product(ProductResponse.builder()
+                            .productId(currentProductDetail.getProduct().getId())
+                            .productName(currentProductDetail.getProduct().getName())
+                            .stock(currentProductDetail.getStock())
+                            .price(currentProductDetail.getPrice())
+                            .isActive(currentProductDetail.getIsActive())
+                            .build())
+                    .build();
+        }).toList();
+
+        return OrderResponse.builder()
+                .orderId(lastInsertOrderId)
+                .transactionDate(order.getDate())
+                .cashier(cashierResponse)
+                .listOrderDetail(orderDetailResponses)
+                .build();
+    }
+
+    @Override
+    public Order getOrderById(Integer id) {
+        Optional<Order> orderOptional = orderRepository.getOrderById(id);
+        return orderOptional.orElse(null);
+    }
+
+    @Override
+    public OrderResponse getOrderResponseById(Integer id) {
+        Optional<Order> order = orderRepository.getOrderById(id);
+        if (order.isPresent()) {
+            Order orders = order.get();
+            OrderDetail orderDetail = orders.getOrderDetail().stream().findFirst().orElse(null);
+            CashierResponse cashierResponse = cashierService.getById(orders.getCashier().getId());
+            List<OrderDetailResponse> orderDetailResponses = orders.getOrderDetail()
+                    .stream()
+                    .map(orderDetails -> {
+                        orderDetails.setOrder(orders);
+                        ProductDetail currentProductDetail = orderDetails.getProductDetail();
+                        return OrderDetailResponse.builder()
+                                .orderDetailId(orderDetails.getId())
+                                .quantity(orderDetails.getQuantity())
+                                .product(ProductResponse.builder()
+                                        .productId(currentProductDetail.getProduct().getId())
+                                        .productName(currentProductDetail.getProduct().getName())
+                                        .stock(currentProductDetail.getStock())
+                                        .price(currentProductDetail.getPrice())
+                                        .isActive(currentProductDetail.getIsActive())
+                                        .build())
+                                .build();
+                    }).toList();
+            if (orderDetail != null) {
+                return OrderResponse.builder()
+                        .orderId(orders.getId())
+                        .transactionDate(orders.getDate())
+                        .listOrderDetail(orderDetailResponses)
+                        .cashier(cashierResponse)
+                        .build();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public List<OrderResponse> getAllOrder() {
+        List<Order> orders = orderRepository.getAllOrder();
+        return  orders.stream()
+                .map(order -> {
+                    CashierResponse cashierResponse = cashierService.getById(order.getCashier().getId());
+                    List<OrderDetailResponse> orderDetailResponses = order.getOrderDetail()
+                            .stream()
+                            .map(orderDetail -> {
+                                orderDetail.setOrder(order);
+                                ProductDetail productDetail = orderDetail.getProductDetail();
+                                return OrderDetailResponse.builder()
+                                        .orderDetailId(orderDetail.getId())
+                                        .quantity(orderDetail.getQuantity())
+                                        .product(ProductResponse.builder()
+                                                .productId(productDetail.getProduct().getId())
+                                                .productName(productDetail.getProduct().getName())
+                                                .stock(productDetail.getStock())
+                                                .price(productDetail.getPrice())
+                                                .isActive(productDetail.getIsActive())
+                                                .build())
+                                        .build();
+                            }).toList();
+                    return  OrderResponse.builder()
+                            .orderId(order.getId())
+                            .cashier(cashierResponse)
+                            .listOrderDetail(orderDetailResponses)
+                            .transactionDate(order.getDate())
+                            .build();
+
+                }).collect(Collectors.toList());
+    }
+
+    @Override
+    public Page<OrderResponse> getAllWithPagination(Integer page, Integer size, Integer cashierId) {
+        Specification<Order> specification = (root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (cashierId != null) {
+                Join<Order, Cashier> cashierJoin = root.join("cashier", JoinType.INNER);
+                predicates.add(criteriaBuilder.equal(cashierJoin.get("id"), cashierId));
+            }
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+        Pageable pageable = PageRequest.of(page, size);
+
+        Page<Order> orders = orderRepository.findAll(specification, pageable);
+
+        List<OrderResponse> orderResponses = orders.getContent().stream()
+                .flatMap(order -> order.getOrderDetail().stream()
+                        .map(orderDetail -> {
+                            CashierResponse cashierResponse = cashierService.getById(order.getCashier().getId());
+                            List<OrderDetailResponse> orderDetailResponses = order.getOrderDetail()
+                                    .stream()
+                                    .map(orderDetails -> {
+                                        orderDetails.setOrder(order);
+                                        ProductDetail productDetail = orderDetails.getProductDetail();
+                                        return OrderDetailResponse.builder()
+                                                .orderDetailId(orderDetails.getId())
+                                                .quantity(orderDetails.getQuantity())
+                                                .product(ProductResponse.builder()
+                                                        .productId(productDetail.getProduct().getId())
+                                                        .productName(productDetail.getProduct().getName())
+                                                        .stock(productDetail.getStock())
+                                                        .price(productDetail.getPrice())
+                                                        .isActive(productDetail.getIsActive())
+                                                        .build())
+                                                .build();
+                                    }).toList();
+                            return OrderResponse.builder()
+                                    .orderId(order.getId())
+                                    .transactionDate(order.getDate())
+                                    .cashier(cashierResponse)
+                                    .listOrderDetail(orderDetailResponses)
+                                    .build();
+                        })).collect(Collectors.toList());
+        return new PageImpl<>(orderResponses, pageable, orders.getTotalElements());
+    }
+}
